870578a98cebbacdf531e0fb3507c95e89713d10
EnigmaticDoll <webmaster@cygnus.stream>
2025-11-19 04:40:14 +0900

    feat: Add player movement with animations (#2)

---
+++ Assets/Scripts/PlayerMovementBehaviour.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speedJog;
+    [SerializeField] private float speedSprint;
+    [SerializeField] private float jumpInitialVerticalSpeed;
+    [SerializeField] private float gravity;
+    [SerializeField] private float sprintTimeMax;
+    [SerializeField] private float sprintTimeRechargeCoefficient;
+
+    private float verticalSpeed;
+    private float sprintTimeLeft;
+    private bool isWaitingSprintFullRecharge;
+
+    CharacterController controller;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+        verticalSpeed = 0;
+        sprintTimeLeft = sprintTimeMax;
+        isWaitingSprintFullRecharge = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float upDown = (Input.GetKey(KeyCode.W) ? 1f : 0f) - (Input.GetKey(KeyCode.S) ? 1f : 0f);
+        float leftRight = (Input.GetKey(KeyCode.D) ? 1f : 0f) - (Input.GetKey(KeyCode.A) ? 1f : 0f);
+        bool isSprintPressed = Input.GetKey(KeyCode.LeftShift);
+        bool isJumpPressed = Input.GetKey(KeyCode.Space);
+
+        bool isGrounded = 0 == transform.position.y; // ignore controller.isGrounded, and just use y coordinate value.
+
+        Vector2 horizontalDirection = new Vector2(upDown, leftRight).normalized;
+
+        if (sprintTimeLeft == 0f) isWaitingSprintFullRecharge = true;
+        if (sprintTimeMax == sprintTimeLeft) isWaitingSprintFullRecharge = false;
+        bool isSprinting = isSprintPressed && !isWaitingSprintFullRecharge && isGrounded && !isJumpPressed && (0 == horizontalDirection.magnitude);
+        sprintTimeLeft = Mathf.Clamp(sprintTimeLeft + (isSprinting ? -Time.deltaTime : sprintTimeRechargeCoefficient * Time.deltaTime), 0f, sprintTimeMax);
+        Vector2 horizontalVelocity = horizontalDirection * (isSprinting ? speedSprint : speedJog);
+        // TODO get camera from game manager and rotate axis
+
+        if (isGrounded)
+        {
+            verticalSpeed = isJumpPressed ? jumpInitialVerticalSpeed : 0f;
+        }
+        else
+        {
+            verticalSpeed -= gravity * Time.deltaTime;
+            verticalSpeed = Mathf.Max(verticalSpeed, -transform.position.y / Time.deltaTime);
+        }
+
+        if (0 != horizontalDirection.magnitude) transform.rotation = Quaternion.LookRotation(new Vector3(horizontalDirection.x, 0, horizontalDirection.y));
+        controller.Move(new Vector3(horizontalVelocity.x, verticalSpeed, horizontalVelocity.y) * Time.deltaTime);
+
+        int speedstep = (0 == horizontalDirection.magnitude) ? 0 : isSprinting ? 2 : 1;
+        animator.SetInteger("speedstep", speedstep);
+        animator.SetBool("isGrounded", isGrounded);
+    }
+}
